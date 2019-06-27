2082eb68ed7ea50c56a648a30a340232183e25d3
Yugo Yagita <2000ygkn0713@gmail.com>
2019-06-27 17:31:25 +0900

    bigger font size

--- script/index.js
+++ script/index.js
@@ -5,8 +5,8 @@ window.addEventListener("load", () => {
   assets.querySelectorAll("#likes > li").forEach((_, i) => {
     const likeContents = document.querySelectorAll("#like > section");
     const like = document.createElement("a-plane");
-    like.setAttribute("height", "0.5");
-    like.setAttribute("width", "0.5");
+    like.setAttribute("height", "0.7");
+    like.setAttribute("width", "0.7");
     like.setAttribute(
       "material",
       `shader: html;
@@ -14,7 +14,7 @@ window.addEventListener("load", () => {
        fps: 0;`
     );
     like.setAttribute("rotation", "-90 0 0");
-    like.setAttribute("position", `${0.7 * i} 0 2.5`);
+    like.setAttribute("position", `${0.8 * i} 0 2.5`);
     like.addEventListener("click", () => {
       console.log("clicked", i);
       likeContents.forEach((likeContent, contentIndex) => {
